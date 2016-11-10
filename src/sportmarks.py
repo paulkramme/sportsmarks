a381e10a4e3a0a54bf45d8ea81257eb442e5e789
Paul Kramme <pjkramme@gmail.com>
2016-11-10 21:19:59 +0100

    Add main function

---
+++ src/sportmarks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def mark(hits, distance):
+	points = hits * 3
+	return points
+
+
+def main():
+	name = input("Name? ")
+	hits = int(input("Anzahl getroffen? "))
+	distance = int(input("Entfernung zum Korb? "))
+	finalmark = mark(hits, distance)
+	with open("marklist.csv", "a") as f:
+		f.write(name + " hits=" + str(hits) + " on distance=" + str(distance) + " equals final mark " + str(finalmark) + '\n')
+	print("End program.")
+
+if __name__ == __name__:
+	main()
