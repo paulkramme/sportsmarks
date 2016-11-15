4d2b9f2a2fef7a864b4e5439715e42dac887c1e3
Paul Kramme <pjkramme@gmail.com>
2016-11-15 10:09:09 +0100

    Add keyboard interrupt

--- sportmarks.py
+++ sportmarks.py
@@ -17,32 +17,35 @@ def exists():
 
 
 def main():
-	finalmark = 0
-	hits = 0
-	throw_count = 5
-	i = 1
-	while True:
-		name = input("Name? ")
-		if name != '':
-			break
-		else:
-			pass
-	while i <= throw_count:
-		hit = input("Getroffen? (y/n) ")
-		if hit == "y" or hits == "ja":
-			distance = int(input("Entfernung zum Korb? "))
-			hits = hits + 1
-			finalmark = mark(hits, distance)
-			i = i + 1
-		elif hit == "n" or hits == "nein":
-			i = i + 1
-		else:
-			print("Please state 'y' or 'n' or 'ja' or 'nein'.")
-		print(hits)
-	exists()
-	with open("marklist.csv", "a") as f:
-		f.write(name + "," + str(hits) + "," + str(finalmark) + '\n')
-	print("End program.")
+	try:
+		finalmark = 0
+		hits = 0
+		throw_count = 5
+		i = 1
+		while True:
+			name = input("Name? ")
+			if name != '':
+				break
+			else:
+				pass
+		while i <= throw_count:
+			hit = input("Getroffen? (y/n) ")
+			if hit == "y" or hits == "ja":
+				distance = int(input("Entfernung zum Korb? "))
+				hits = hits + 1
+				finalmark = mark(hits, distance)
+				i = i + 1
+			elif hit == "n" or hits == "nein":
+				i = i + 1
+			else:
+				print("Please state 'y' or 'n' or 'ja' or 'nein'.")
+			print(hits)
+		exists()
+		with open("marklist.csv", "a") as f:
+			f.write(name + "," + str(hits) + "," + str(finalmark) + '\n')
+		print("End program.")
+	except KeyboardInterrupt:
+		print("\nAbort.")
 
 
 if __name__ == __name__:
